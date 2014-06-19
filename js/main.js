31f79d9ef95d9d2a393863b1dbfedf7bf82f0a68
uma <u.krishnan2012@gmail.com>
2014-06-19 11:08:18 -0700

    touch enabled

--- js/main.js
+++ js/main.js
@@ -13,30 +13,32 @@
 define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
     var ImageSurface = require('famous/surfaces/ImageSurface');
-    var Modifier = require('famous/core/Modifier');
+    var StateModifier = require('famous/modifiers/StateModifier');
     var FlexibleLayout = require('famous/views/FlexibleLayout');
-
 	var mainContext = Engine.createContext();
-
-
     var initialRatios = [];
     var finalRatios = [];
-    var No= 40;
+    var No= 30;
+    if (window.innerWidth < 600)
+        No = 15;
     var flex = new FlexibleLayout({
         ratios : initialRatios
     });
-
+    var flexMod = new StateModifier({
+            origin: [0,0.05]
+    });
     var surfaces = [];
+    var size = [undefined, 200];
     for (var i = 0; i < No; i++) {
-        size = [undefined, 200]
-        surfaces.push(new ImageSurface({
+        var temp = new ImageSurface({
             size: size,
-	    content:'images/img'+(i%12)+'.jpg',
+	        content:'images/img'+(i%12)+'.jpg',
             properties: {
  //               backgroundColor: "hsl(" + (i * 360 / 50) + ", 100%, 50%)",
                 border:"solid 1px black"
             }
-        }));
+        });
+        surfaces.push(temp);
         initialRatios.push(1);
         finalRatios.push(1);
     }
@@ -56,16 +58,36 @@ define(function(require, exports, module) {
     var toggle = false;
     Engine.on('click', function(){
         var ratios = toggle ? initialRatios : finalRatios;
-        flex.setRatios(ratios, {curve : 'easeOut', duration : 500});
+        flex.setRatios(ratios, {curve : 'easeOut', duration : 100});
         toggle = !toggle;
     });
     Engine.on('mousemove', function(e){
         var ratios = createRatio(e.clientX);
+         console.log('touch start' + e.clientX);
         flex.setRatios(ratios, {curve : 'easeOut', duration : 30});
+    });
+    Engine.on('touchstart',function(e) {
+        var ratios = createRatio(e.touches[0].clientX);
+        console.log('touch start' + e.touches[0].clientX);
+        flex.setRatios(ratios);
+       // , {curve : 'easeOut', duration : 30});
+    });
+    Engine.on('touchmove', function(e){
+        var ratios = createRatio(e.touches[0].clientX);
+        console.log('touch start' + e.touches[0].clientX);
+        flex.setRatios(ratios);
+        // {curve : 'easeOut', duration : 30});
     });
     Engine.on('mouseleave', function(e){
         var ratios = initialRatios;
+        console.log('touch start' + e.clientX);
         flex.setRatios(ratios, {curve : 'easeOut', duration : 500});
     });
-    mainContext.add(flex);
+    Engine.on('touchend', function(e){
+        var ratios = initialRatios;
+        console.log('touch end');
+        flex.setRatios(ratios);
+        //, {curve : 'easeOut', duration : 500});
+    });
+    mainContext.add(flexMod).add(flex);
 });
